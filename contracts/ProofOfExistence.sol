535d1312f3dd0c3497737bff34257c02ab885840
Mark Muskardin <mark.muskardin@gmail.com>
2017-11-20 10:52:54 -0800

    added proof of existence contract

---
+++ contracts/ProofOfExistence.sol
@@ -0,0 +1,27 @@
+pragma solidity ^0.4.18;
+
+contract ProofOfExistence {
+  mapping (bytes32 => bool) private proofs;
+
+  function storeProof(bytes32 proof) {
+    proofs[proof] = true;
+  }
+
+  function notarize(string document) {
+    var proof = proofFor(document);
+    storeProof(proof);
+  }
+
+  function proofFor(string document) constant returns (bytes32) {
+    return sha256(document);
+  }
+
+  function checkDocument(string document) constant returns (bool) {
+    var proof = proofFor(document);
+    return hasProof(proof);
+  }
+
+  function hasProof(bytes32 proof) constant returns(bool) {
+    return proofs[proof];
+  }
+}
