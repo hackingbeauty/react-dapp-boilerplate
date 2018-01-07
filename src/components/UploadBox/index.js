a28b165c29df319bcf7e3fa279aea3ed29d7a9c9
Mark Muskardin <mark.muskardin@gmail.com>
2018-01-07 14:21:11 -0800

    more clean up

--- src/components/UploadBox/index.js
+++
@@ -1,1 +0,0 @@
-export UploadBox from './UploadBox'
--- src/containers/HomeView/index.js
+++ src/containers/HomeView/index.js
@@ -2,8 +2,6 @@ import React, { Component }   from 'react'
 import { connect }            from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { withRouter }         from 'react-router-dom';
-import { UploadBox }          from 'components/UploadBox'
-import Button                  from 'components/Button'
 
 /* component styles */
 import { styles } from './styles.scss';
@@ -14,25 +12,9 @@ import * as uiActionCreators from 'core/actions/actions-ui';
 class HomeView extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      fileAdded: false
-    }
-  }
-
-  onDrop=() => {
-    this.setState({
-      fileAdded: true
-    })
-  }
-
-  registerAsset=() => {    
-    const { history } = this.props
-    history.push('/register')
   }
 
   render() {
-    const { fileAdded } = this.state
-
     return (
       <div className={styles}>   
         <div id="home-view">   
