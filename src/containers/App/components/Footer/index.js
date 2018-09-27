2a17a1f8e65ded1c78c4c057d9edae80b00c17ef
Mark Muskardin <mark.muskardin@gmail.com>
2018-09-27 11:58:53 -0700

    Removed dead code

--- src/containers/App/components/Footer/index.js
+++ src/containers/App/components/Footer/index.js
@@ -1,15 +1,8 @@
 import React, { Component } from 'react'
-import PropTypes            from 'prop-types'
-import { withRouter }       from 'react-router-dom'
 import BottomNavigation     from 'components/BottomNavigation'
 import { styles }           from './styles.scss'
 
 class Footer extends Component {
-  onClick = () => {
-    const { history } = this.props
-    history.push('/record')
-  }
-
   render() {
     return (
       <div className={styles}>
@@ -21,8 +14,4 @@ class Footer extends Component {
   }
 }
 
-Footer.propTypes = {
-  history: PropTypes.shape({}).isRequired
-}
-
-export default withRouter(Footer)
+export default Footer
