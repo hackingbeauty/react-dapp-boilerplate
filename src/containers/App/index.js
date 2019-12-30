01b11e6a240e66700077cb7614b24c3026376477
Mark Muskardin <mark.muskardin@gmail.com>
2019-12-30 10:17:44 -0800

    back-tracked changes to boilerplate

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,8 +1,4 @@
 import React, { Component }   from 'react'
-import PropTypes              from 'prop-types'
-import { connect }            from 'react-redux'
-import { bindActionCreators } from 'redux'
-import * as providerActionCreators from 'core/actions/actions-provider'
 import { MuiThemeProvider }   from '@material-ui/core/styles'
 import {
   HashRouter,
@@ -10,7 +6,6 @@ import {
   Redirect,
   Switch
 } from 'react-router-dom'
-import ProgressIndicator        from 'components/ProgressIndicator'
 import theme                    from 'configs/theme/config-theme'
 import HomeView                 from 'containers/HomeView'
 import Header                   from './components/Header'
@@ -19,28 +14,7 @@ import Footer                   from './components/Footer'
 import './styles.scss' // global styles
 
 class App extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      web3Provider: null
-    }
-  }
-
-  static getDerivedStateFromProps(nextProps) {
-    return {
-      web3Provider: nextProps.provider.web3Provider
-    }
-  }
-
-  componentDidMount() {
-    const { actions } = this.props
-    actions.provider.setProvider()
-  }
-
   render() {
-    const { web3Provider } = this.state
-    const isLoading = web3Provider === null
-
     return (
       <MuiThemeProvider theme={theme}>
         <HashRouter>
@@ -48,14 +22,10 @@ class App extends Component {
             <Header />
             <Footer />
             <div className="app-shell">
-              { isLoading ? (
-                <ProgressIndicator color="secondary" size={50} />
-              ) : (
-                <Switch>
-                  <Route path="/home" component={HomeView} />
-                  <Redirect from="/" to="/home" />
-                </Switch>
-              )}
+              <Switch>
+                <Route path="/home" component={HomeView} />
+                <Redirect from="/" to="/home" />
+              </Switch>
             </div>
           </div>
         </HashRouter>
@@ -64,22 +34,4 @@ class App extends Component {
   }
 }
 
-function mapStateToProps(state) {
-  return {
-    provider: state.provider
-  }
-}
-
-function mapDispatchToProps(dispatch) {
-  return {
-    actions: {
-      provider: bindActionCreators(providerActionCreators, dispatch)
-    }
-  }
-}
-
-App.propTypes = {
-  actions: PropTypes.shape({}).isRequired
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(App)
+export default App
