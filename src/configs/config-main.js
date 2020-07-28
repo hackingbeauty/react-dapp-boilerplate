ecfd877b7a3fbee889d249e76b04bcb3f832a3a8
Mark Muskardin <mark.muskardin@gmail.com>
2020-07-28 06:25:01 -0700

    tweaks

--- src/configs/config-main.js
+++ src/configs/config-main.js
@@ -1,8 +1,8 @@
 /* Main app configs go here */
 
 export const appConfig = {
-  name: 'Your React DApp Name',
-  shortName: 'YourReactDApp',
-  description: 'An expert foundational React boilerplate for Ethereum DApps',
+  name: 'Smart Green Bonds',
+  shortName: 'SmartGreenBond',
+  description: 'Sustainability-linked green bonds on Ethereum',
   splashScreenBackground: '#ffffff'
 }
